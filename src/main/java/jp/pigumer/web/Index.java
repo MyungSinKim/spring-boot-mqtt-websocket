8bb3bd66c97031aa570fbb1378c5cccb060148e3
takesection <takesection@gmail.com>
2015-07-15 00:44:52 +0900

    コメントとimportの再編成

--- src/main/java/jp/pigumer/web/Index.java
+++ src/main/java/jp/pigumer/web/Index.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @ComponentScan
 @Controller
